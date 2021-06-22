bbf540aab2117ece41a771648fb7d74bbc772dec
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2021-06-22 16:18:35 +0200

    mpi main

--- test/mpi_runner/gtest_main_mpi.cpp
+++ test/mpi_runner/gtest_main_mpi.cpp
@@ -7,16 +7,45 @@
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include "./mpi_setup.hpp"
+#include <fstream>
+#include <hwmalloc/mpi/error.hpp>
+#include <gtest/gtest.h>
+#include "mpi_listener.hpp"
 
 GTEST_API_ int
 main(int argc, char** argv)
 {
-    init_mpi(argc, argv);
+    int required = MPI_THREAD_MULTIPLE;
+    int provided;
+    int init_result = MPI_Init_thread(&argc, &argv, required, &provided);
+    if (init_result == MPI_ERR_OTHER) throw std::runtime_error("MPI init failed");
+    if (provided < required)
+        throw std::runtime_error("MPI does not support required threading level");
 
-    auto result = run_tests();
+    // printf("Running main() from %s\n", __FILE__);
+    testing::InitGoogleTest(&argc, argv);
 
-    finalize_mpi();
+    // set up a custom listener that prints messages in an MPI-friendly way
+    auto& listeners = testing::UnitTest::GetInstance()->listeners();
+    // first delete the original printer
+    delete listeners.Release(listeners.default_result_printer());
+    // now add our custom printer
+    listeners.Append(new mpi_listener("results_tests"));
 
-    return result;
+    // record the local return value for tests run on this mpi rank
+    //      0 : success
+    //      1 : failure
+    auto result = RUN_ALL_TESTS();
+
+    // perform global collective, to ensure that all ranks return
+    // the same exit code
+    decltype(result) global_result{};
+    MPI_Allreduce(&result, &global_result, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+    return global_result;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+
+    return global_result;
 }
