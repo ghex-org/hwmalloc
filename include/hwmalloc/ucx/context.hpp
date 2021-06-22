2bf010274f0d21b95b8d3861678f21878ed1139c
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2021-06-22 12:18:51 +0200

    context outline

--- include/hwmalloc/ucx/context.hpp
+++ include/hwmalloc/ucx/context.hpp
@@ -16,5 +16,78 @@ namespace hwmalloc
 {
 namespace ucx
 {
+struct handle
+{
+    void*       m_ptr;
+    std::size_t m_size;
+};
+
+struct region
+{
+    using handle_type = handle;
+    ucp_context_h m_ucp_context;
+    void*         m_ptr;
+    std::size_t   m_size;
+    ucp_mem_h     m_memh;
+
+    region(ucp_context_h ctxt, void* ptr, std::size_t size)
+    : m_ucp_context{ctxt}
+    , m_ptr{ptr}
+    , m_size{size}
+    {
+        ucp_mem_map_params_t params;
+
+        params.field_mask = UCP_MEM_MAP_PARAM_FIELD_ADDRESS | UCP_MEM_MAP_PARAM_FIELD_LENGTH
+            //| UCP_MEM_MAP_PARAM_FIELD_FLAGS
+            //| UCP_MEM_MAP_PARAM_FIELD_MEMORY_TYPE
+            ;
+
+        params.address = ptr;
+        params.length = size;
+
+        //ucs_status_t
+        ucs_status_t status = ucp_mem_map(m_ucp_context, &params, &m_memh);
+    }
+
+    region(region const&) = delete;
+
+    region(region&& r) noexcept
+    : m_ucp_context{r.m_ucp_context}
+    , m_ptr{std::exchange(r.m_ptr, nullptr)}
+    , m_size{r.m_size}
+    , m_memh{r.m_memh}
+    {
+    }
+
+    ~region()
+    {
+        if (m_ptr) { ucs_status_t status = ucp_mem_unmap(m_ucp_context, m_memh); }
+    }
+
+    // get a handle to some portion of the region
+    handle_type get_handle(std::size_t offset, std::size_t size)
+    {
+        return {(void*)((char*)m_ptr + offset), size};
+    }
+};
+
+struct context
+{
+    ucp_context_h m_ucp_context;
+
+    context() = default;
+    context(context const&) = delete;
+    context(context&&) = delete;
+    ~context() {}
+
+    region make_region(void* ptr, std::size_t size) { return {m_ucp_context, ptr, size}; }
+};
+
+auto
+register_memory(context& c, void* ptr, std::size_t size)
+{
+    return c.make_region(ptr, size);
+}
+
 } // namespace ucx
 } // namespace hwmalloc
