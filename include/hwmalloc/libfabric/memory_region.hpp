a5c3c4099b8c427706b9056581c452bcf4c6be24
John Biddiscombe <biddisco@cscs.ch>
2021-07-30 15:18:48 +0200

    Fixes to allow hwmalloc::libfabric functionality inside hpx
    HPX has requirements for user managed memory and regions (memory
    handles) need to hold extra information so that they can be
    correctly unregistered without double free/unregister etc.

--- include/hwmalloc/libfabric/memory_region.hpp
+++ include/hwmalloc/libfabric/memory_region.hpp
@@ -22,6 +22,13 @@
 #include <memory>
 #include <utility>
 //
+#if __has_include(<hpx/debugging/print.hpp>)
+#   include <hpx/debugging/print.hpp>
+    // cppcheck-suppress ConfigurationNotChecked
+    static hpx::debug::enable_print<true> mrn_deb("REGION_");
+#  define has_debug 1
+#else
+#endif
 
 namespace hwmalloc
 {
@@ -34,6 +41,10 @@ struct region_provider
     // The internal memory region handle
     typedef struct fid_mr     provider_region;
     typedef struct fid_domain provider_domain;
+
+//    fi_mr_reg(struct fid_domain *domain, const void *buf, size_t len,
+//          uint64_t acs, uint64_t offset, uint64_t requested_key,
+//          uint64_t flags, struct fid_mr **mr, void *context)
 
     // register region
     template<typename... Args>
@@ -60,26 +71,59 @@ struct region_provider
 
 // --------------------------------------------------------------------
 // This is a handle to a small chunk of memory that has been registered
-// as part of a much larger allocation (a segment)
+// as part of a much larger allocation (a memory_segment)
 struct memory_handle
 {
-    using provider_domain = region_provider::provider_domain;
+    // --------------------------------------------------------------------
     using provider_region = region_provider::provider_region;
 
+    // --------------------------------------------------------------------
     // Default constructor creates unusable handle(region)
     memory_handle() {}
     memory_handle(memory_handle const&) noexcept = default;
     memory_handle& operator=(memory_handle const&) noexcept = default;
-    memory_handle(memory_handle&&) noexcept = default;
 
-    memory_handle(provider_region* region, char* addr, std::size_t size)
-    : local_key_{region_provider::get_local_key(region)}
-    , remote_key_{region_provider::get_remote_key(region)}
-    , address_{addr}
-    , size_{size}
-    , used_space_{0}
+    memory_handle(provider_region* region, unsigned char* addr, std::size_t size, uint32_t flags) noexcept
+        : address_{addr}
+        , region_{region}
+        , local_key_{region_provider::get_local_key(region)}
+        , remote_key_{region_provider::get_remote_key(region)}
+        , size_{size}
+        , used_space_{0}
+        , flags_{flags}
     {
     }
+
+    // --------------------------------------------------------------------
+    // move constructor, clear other region so that it is not unregistered twice
+    memory_handle(memory_handle&& other) noexcept
+        : address_{other.address_}
+        , region_{std::exchange(other.region_, nullptr)}
+        , local_key_{other.local_key_}
+        , remote_key_{other.remote_key_}
+        , size_{other.size_}
+        , used_space_{other.used_space_}
+        , flags_{other.flags_}
+    {
+    }
+
+    // --------------------------------------------------------------------
+    // move assignment, clear other region so that it is not unregistered twice
+    memory_handle& operator=(memory_handle&& other) noexcept
+    {
+        address_ = other.address_;
+        region_ = std::exchange(other.region_, nullptr);
+        local_key_ = other.local_key_;
+        remote_key_ = other.remote_key_;
+        size_ = other.size_;
+        used_space_ = other.used_space_;
+        flags_ = other.flags_;
+        return *this;
+    }
+
+    // --------------------------------------------------------------------
+    // Return the address of this memory region block.
+    inline unsigned char* get_address(void) const { return address_; }
 
     // --------------------------------------------------------------------
     // Get the local descriptor of the memory region.
@@ -88,12 +132,6 @@ struct memory_handle
     // --------------------------------------------------------------------
     // Get the remote key of the memory region.
     inline uint64_t get_remote_key(void) const { return remote_key_; }
-
-    // --------------------------------------------------------------------
-    // return the address of this memory region block. If this
-    // is a partial region, then the address will be offset from the
-    // base address
-    inline char* get_address(void) const { return address_; }
 
     // --------------------------------------------------------------------
     // Get the size of the memory chunk usable by this memory region,
@@ -109,22 +147,80 @@ struct memory_handle
     // Set the size used by a message in the memory region.
     inline void set_message_length(uint32_t length) { used_space_ = length; }
 
-  protected:
-    // we may be a piece of a larger region, this gives the start address
-    // of this piece of the region. This is the address that should be used for data
-    // storage
-    char* address_;
+    // --------------------------------------------------------------------
+    // A user allocated region uses memory allocted by the user.
+    // on destruction, the memory is unregistered, but not deleted
+    inline void set_user_region() { flags_ |= registration_flags::memory_user; }
 
-    // local/remote keys cached from fi_mr_desc
+    inline bool get_user_region() const { return (flags_ & registration_flags::memory_user) == registration_flags::memory_user; }
+/*
+    // --------------------------------------------------------------------
+    // A temp region is one that the memory pool is not managing
+    // so it is unregistered and deleted when returned to the pool and not reused
+    inline void set_temp_region() { flags_ |= BLOCK_TEMP; }
+
+    inline bool get_temp_region() const { return (flags_ & BLOCK_TEMP) == BLOCK_TEMP; }
+
+    // --------------------------------------------------------------------
+    // a partial region is a subregion of a larger memory region
+    // on destruction, it is not unregistered or deleted as the 'parent' region
+    // will delete many partial regions on destruction
+    inline void set_partial_region() { flags_ |= BLOCK_PARTIAL; }
+
+    inline bool get_partial_region() const { return (flags_ & BLOCK_PARTIAL) == BLOCK_PARTIAL; }
+*/
+
+    // --------------------------------------------------------------------
+    void release_region() noexcept
+    {
+        region_ = nullptr;
+    }
+
+    // --------------------------------------------------------------------
+    // return the underlying libfabric region handle
+    inline provider_region* get_region() const { return region_; }
+
+    // --------------------------------------------------------------------
+    friend std::ostream& operator<<(std::ostream& os, memory_handle const& region)
+    {
+#if has_debug
+        // clang-format off
+        os << "region "      << hpx::debug::ptr(&region)
+           << " fi_region "  << hpx::debug::ptr(region.region_)
+           << " address "    << hpx::debug::ptr(region.address_)
+           << " size "       << hpx::debug::hex<6>(region.size_)
+           << " used_space " << hpx::debug::hex<6>(region.used_space_)
+           << " local key "  << hpx::debug::ptr(region.local_key_)
+           << " remote key " << hpx::debug::ptr(region.remote_key_)
+           << " flags "      << hpx::debug::bin<8>(region.flags_);
+        // clang-format on
+#endif
+        return os;
+    }
+
+  protected:
+    // This gives the start address of this region.
+    // This is the address that should be used for data storage
+    unsigned char * address_;
+
+    // The hardware level handle to the region (as returned from libfabric fi_mr_reg)
+    provider_region* region_;
+
+    // local/remote keys cached from region (fi_mr_desc)
     void*    local_key_;
     uint64_t remote_key_;
 
-    // The size of the memory buffer, if this is a partial region
-    // it will be smaller than the value returned by region_->length
+    // The (maximum available) size of the memory buffer
     uint64_t size_;
 
-    // space used by a message in the memory region.
-    uint64_t used_space_;
+    // Space used by a message in the memory region.
+    // This may be smaller/less than the size available if more space
+    // was allocated than it turns out was needed
+    mutable uint64_t used_space_;
+
+    // flags to control lifetime of blocks
+    uint32_t flags_;
+
 };
 
 // --------------------------------------------------------------------
@@ -140,27 +236,10 @@ struct memory_segment : public memory_handle
     using handle_type = memory_handle;
 
     // --------------------------------------------------------------------
-    // flags used for management of lifetime
-    enum
-    {
-        BLOCK_USER = 1,
-        BLOCK_TEMP = 2,
-        BLOCK_PARTIAL = 4,
-    };
-
-    //        memory_segment()
-    //            : memory_handle()
-    //            , region_(nullptr)
-    //            , base_addr_(nullptr)
-    //            , flags_(0)
-    //        {}
-
     memory_segment(
-        provider_region* region, char* address, char* base_address, uint64_t size, uint32_t flags)
-    : memory_handle(region, address, size)
-    , region_(region)
+        provider_region* region, unsigned char* address, unsigned char* base_address, uint64_t size, registration_flags flags)
+    : memory_handle(region, address, size, flags)
     , base_addr_(base_address)
-    , flags_(flags)
     {
     }
 
@@ -168,9 +247,7 @@ struct memory_segment : public memory_handle
     // move constructor, clear other region
     memory_segment(memory_segment&& other) noexcept
     : memory_handle(std::move(other))
-    , region_{std::exchange(other.region_, nullptr)}
     , base_addr_{std::exchange(other.base_addr_, nullptr)}
-    , flags_{other.flags_}
     {
     }
 
@@ -180,38 +257,43 @@ struct memory_segment : public memory_handle
     {
         memory_handle(std::move(other));
         region_ = std::exchange(other.region_, nullptr);
-        base_addr_ = std::exchange(other.base_addr_, nullptr);
-        flags_ = other.flags_;
+        return *this;
     }
 
     // --------------------------------------------------------------------
     // construct a memory region object by registering an existing address buffer
-    memory_segment(provider_domain* pd, const void* buffer, const uint64_t length)
+    // we do not cache local/remote keys here because memory segments are only
+    // used by the heap to store chunks and the user will always receive
+    // a memory_handle - which does have keys cached
+    memory_segment(provider_domain* pd, const void* buffer, const uint64_t length, registration_flags flags)
     {
-        memory_handle::address_ = static_cast<char*>(const_cast<void*>(buffer));
-        memory_handle::size_ = length;
-        memory_handle::used_space_ = length;
-        //
+        address_ = static_cast<unsigned char*>(const_cast<void*>(buffer));
+        size_ = length;
+        used_space_ = length;
         region_ = nullptr;
-        base_addr_ = address_;
-        flags_ = BLOCK_USER;
+        flags_ = flags;
+        //
+        base_addr_ = memory_handle::address_;
 
         int ret = region_provider::register_memory(pd, const_cast<void*>(buffer), length,
             region_provider::flags(), 0, (uint64_t)address_, 0, &(region_), nullptr);
 
+#if has_debug
         if (ret)
         {
-            //                memr_deb.debug("error registering region ",
-            //                    alloctools::debug::ptr(buffer), alloctools::debug::hex<6>(length));
+            mrn_deb.error(hpx::debug::str<>("Registering region"),
+                hpx::debug::ptr(buffer), hpx::debug::hex<6>(length));
         }
         else
         {
-            //                memr_deb.trace("OK registering region ",
-            //                    alloctools::debug::ptr(buffer), alloctools::debug::ptr(address_), "desc ",
-            //                    alloctools::debug::ptr(fi_mr_desc(region_)), "rkey ",
-            //                    alloctools::debug::ptr(fi_mr_key(region_)), "length ",
-            //                    alloctools::debug::hex<6>(size_));
+            // clang-format off
+            mrn_deb.trace(hpx::debug::str<>("Registered region"), hpx::debug::ptr(buffer),
+                          "desc ", hpx::debug::ptr(fi_mr_desc(region_)),
+                          "rkey ", hpx::debug::ptr(fi_mr_key(region_)),
+                          "length ", hpx::debug::hex<6>(size_));
+            // clang-format on
         }
+#endif
     }
 
     // --------------------------------------------------------------------
@@ -220,34 +302,36 @@ struct memory_segment : public memory_handle
     {
         //            if (get_partial_region())
         //                return;
-        release();
+        deregister();
     }
 
     // --------------------------------------------------------------------
     // Deregister the memory region.
     // returns 0 when successful, -1 otherwise
-    int release(void)
+    int deregister(void)
     {
-        if (region_ != nullptr)
+        if (region_ && !get_user_region())
         {
-            //                memr_deb.trace("About to release memory region with local key ",
-            //                    alloctools::debug::ptr(get_local_key()));
+#if has_debug
+            mrn_deb.trace(hpx::debug::str<>("release"), region_);
             // get these before deleting/unregistering (for logging)
+            auto length = mrn_deb.declare_variable<uint64_t>(get_size());
+            (void) length;
+#endif
             const void* buffer = get_base_address();
-            //                auto length = memr_deb.declare_variable<uint64_t>(get_size());
-            //                (void) length;
             //
             if (region_provider::unregister_memory(region_))
             {
-                //                    memr_deb.debug("Error, fi_close mr failed\n");
+#if has_debug
+                mrn_deb.error("fi_close mr failed");
+#endif
                 return -1;
             }
             else
             {
-                //                    memr_deb.trace("deregistered memory region with local key ",
-                //                        alloctools::debug::ptr(get_local_key()), "at address ",
-                //                        alloctools::debug::ptr(buffer), "with length ",
-                //                        alloctools::debug::hex<6>(length));
+#if has_debug
+                mrn_deb.trace(hpx::debug::str<>("deregistered"), region_);
+#endif
             }
             if (!get_user_region())
             {
@@ -260,66 +344,30 @@ struct memory_segment : public memory_handle
 
     handle_type get_handle(std::size_t offset, std::size_t size) const noexcept
     {
-        return memory_handle(region_, base_addr_ + offset, size);
+        return memory_handle(region_, base_addr_ + offset, size, flags_);
     }
-
-    // --------------------------------------------------------------------
-    // return the underlying libfabric region handle
-    inline provider_region* get_region() const { return region_; }
 
     // --------------------------------------------------------------------
     // Get the address of the base memory region.
     // This is the address of the memory allocated from the system
-    inline char* get_base_address(void) const { return base_addr_; }
-
-    // --------------------------------------------------------------------
-    // A user allocated region uses memory allocted by the user.
-    // on destruction, the memory is unregistered, but not deleted
-    inline void set_user_region() { flags_ |= BLOCK_USER; }
-
-    inline bool get_user_region() const { return (flags_ & BLOCK_USER) == BLOCK_USER; }
-
-    // --------------------------------------------------------------------
-    // A temp region is one that the memory pool is not managing
-    // so it is unregistered and deleted when returned to the pool and not reused
-    inline void set_temp_region() { flags_ |= BLOCK_TEMP; }
-
-    inline bool get_temp_region() const { return (flags_ & BLOCK_TEMP) == BLOCK_TEMP; }
-
-    // --------------------------------------------------------------------
-    // a partial region is a subregion of a larger memory region
-    // on destruction, it is not unregistered or deleted as the 'parent' region
-    // will delete many partial regions on destruction
-    inline void set_partial_region() { flags_ |= BLOCK_PARTIAL; }
-
-    inline bool get_partial_region() const { return (flags_ & BLOCK_PARTIAL) == BLOCK_PARTIAL; }
+    inline unsigned char* get_base_address(void) const { return base_addr_; }
 
     // --------------------------------------------------------------------
     friend std::ostream& operator<<(std::ostream& os, memory_segment const& region)
     {
-        //            os << "region " << alloctools::debug::ptr(&region) << " base address "
-        //               << alloctools::debug::ptr(region.base_addr_) << " address "
-        //               << alloctools::debug::ptr(region.address_) << " flags "
-        //               << alloctools::debug::hex<2>(region.flags_) << " size "
-        //               << alloctools::debug::hex<6>(region.size_) << " used_space "
-        //               << alloctools::debug::hex<6>(region.used_space_) << " local key "
-        //               << alloctools::debug::ptr(region.get_local_key()) << " remote key "
-        //               << alloctools::debug::ptr(region.get_remote_key());
+#if has_debug
+        // clang-format off
+        os << *static_cast<const memory_handle*>(&region)
+           << " base address " << hpx::debug::ptr(region.base_addr_);
+        // clang-format on
+#endif
         return os;
     }
 
-    //    private:
-
   public:
-    // The hardware level handle to the region (as returned from libfabric fi_mr_reg)
-    provider_region* region_;
-
-    // if we are part of a larger region, this is the base address of
-    // that larger region
-    char* base_addr_;
-
-    // flags to control lifetime of blocks
-    uint32_t flags_;
+    // this is the base address of the memory registered by this segment
+    // individual memory_handles are offset from this address
+    unsigned char* base_addr_;
 };
 
 // --------------------------------------------------------------------
@@ -350,9 +398,9 @@ struct context
 // --------------------------------------------------------------------
 namespace hwmalloc
 {
-auto
-register_memory(hwmalloc::libfabric::context& c, void* ptr, std::size_t size)
+inline auto
+register_memory(hwmalloc::libfabric::context& c, void * const ptr, std::size_t size, registration_flags flags={})
 {
-    return hwmalloc::libfabric::memory_segment(c.get_domain(), ptr, size);
+    return hwmalloc::libfabric::memory_segment(c.get_domain(), ptr, size, flags);
 }
 } // namespace hwmalloc
